66aa11fc566b1a75988f7aec3360875f6b89de38
Gabe Wyatt <gabe@solarixdigital.com>
2020-02-22 07:54:08 -0800

    Quick rebuild.

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,9 +1,5 @@
 import { ExtensionManager } from './extension-manager';
 declare const extensions: {
-    publicVar: number;
-    sum: (a: any, b: any) => any;
-    multiply: (a: any, b: any) => number;
-    divide: (a: any, b: any) => number;
     manager: ExtensionManager;
 };
 export default extensions;
